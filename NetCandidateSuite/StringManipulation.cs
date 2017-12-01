5e76bd0359b81370f9c2727bec0dba3db53a3eba
Casey O'Brien <caseyobrien2014@gmail.com>
2017-11-30 21:54:47 -0500

    initial commit

---
+++ NetCandidateSuite/StringManipulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCandidateSuite
+{
+    public class StringManipulation
+    {
+        public static char GetCharAt(string input, int charAt)
+        {
+            if (!String.IsNullOrEmpty(input) && charAt > 0)
+            {
+                var mod = charAt % input.Length;
+
+                if(mod == 0)
+                {
+                    return input[input.Length - 1];
+                }
+                else
+                {
+                    return input[mod - 1];
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ NetCandidateSuiteTests/StringManipulationTests.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetCandidateSuite;
+using System;
+
+namespace NetCandidateSuiteTests
+{
+    [TestClass]
+    public class StringManipulationTests
+    {
+        [TestMethod]
+        public void GetCharAtThrowArgumentOutOfRangeExceptionForEmptyInput()
+        {
+            Action action = () => StringManipulation.GetCharAt(String.Empty, 1);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(action);
+        }
+
+        [TestMethod]
+        public void GetCharAtThrowArgumentOutOfRangeExceptionForNullInput()
+        {
+            Action action = () => StringManipulation.GetCharAt(null, 1);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(action);
+        }
+
+        [TestMethod]
+        public void GetCharAtThrowArgumentOutOfRangeExceptionWhereCharAtZero()
+        {
+            Action action = () => StringManipulation.GetCharAt("hello", 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(action);
+        }
+
+        [TestMethod]
+        public void GetCharAtThrowArgumentOutOfRangeExceptionWhereCharAtNegative()
+        {
+            Action action = () => StringManipulation.GetCharAt("hello", -4);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(action);
+        }
+
+        [TestMethod]
+        public void GetCharAtReturnsFirstCharacter()
+        {
+            var result =  StringManipulation.GetCharAt("hello", 1);
+
+            Assert.AreEqual(result, 'h');
+        }
+
+        [TestMethod]
+        public void GetCharAtReturnsFirstCharacterLoops()
+        {
+            var result = StringManipulation.GetCharAt("hello", 11);
+
+            Assert.AreEqual(result, 'h');
+        }
+
+        [TestMethod]
+        public void GetCharAtReturnsLastCharacter()
+        {
+            var result = StringManipulation.GetCharAt("hello", 5);
+
+            Assert.AreEqual(result, 'o');
+        }
+
+        [TestMethod]
+        public void GetCharAtReturnsLastCharacterLoops()
+        {
+            var result = StringManipulation.GetCharAt("hello", 20);
+
+            Assert.AreEqual(result, 'o');
+        }
+
+        [TestMethod]
+        public void GetCharAtReturnsMiddleCharacter()
+        {
+            var result = StringManipulation.GetCharAt("hello", 2);
+
+            Assert.AreEqual(result, 'e');
+        }
+
+        [TestMethod]
+        public void GetCharAtReturnsMiddleCharacterLoops()
+        {
+            var result = StringManipulation.GetCharAt("hello", 102);
+
+            Assert.AreEqual(result, 'e');
+        }
+    }
+}
